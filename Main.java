0b96816375eb7cab9081cbb13f13afd800dfe1d1
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-20 23:48:52 +0200

    TestApp
    Testing the application.

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Checking the leap year
+        System.out.println(NumberOfDaysInMonth.isLeapYear(-2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1,-2020));
+    }
+}
